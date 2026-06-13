961b7192468f3e8742bf5b5ab9b091be371408f7
jorge calle <jorcaza2009@gmail.com>
2026-06-13 11:49:20 -0500

    firebase conf fixed

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 
 // Configuración de tu proyecto
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAgPVgK6BJbxmv7NI2hXN4NdlnyMtfNGKI",
+  authDomain: "dava-nails.firebaseapp.com",
+  projectId: "dava-nails",
+  storageBucket: "dava-nails.firebasestorage.app",
+  messagingSenderId: "92839130296",
+  appId: "1:92839130296:web:b03bbd0af96cc58186704d"
 };
 
 // Inicializar Firebase
